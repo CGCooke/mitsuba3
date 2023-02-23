4e2f6c196957b1701b0a8998d30ec3aa6ab30d91
Nicolas Roussel <n.roussel13@gmail.com>
2023-02-23 13:43:44 +0100

    tmp

--- src/python/python/ad/integrators/prbvolpath.py
+++ src/python/python/ad/integrators/prbvolpath.py
@@ -312,9 +312,11 @@ class PRBVolpathIntegrator(RBIntegrator):
                        δL=None, mode=None):
 
         is_primal = mode == dr.ADMode.Primal
-
         active = mi.Bool(active_medium | active_surface)
+
         medium = dr.select(active, medium, dr.zeros(mi.MediumPtr))
+        # Potentially escaping the medium if this is the current medium's boundary
+        medium[active_surface & si.is_medium_transition()] = ref_interaction.target_medium(ds.d);
 
         ref_interaction = dr.zeros(mi.Interaction3f)
         ref_interaction[active_medium] = mei
@@ -325,9 +327,6 @@ class PRBVolpathIntegrator(RBIntegrator):
         invalid = dr.eq(ds.pdf, 0.0)
         emitter_val[invalid] = 0.0
         active &= ~invalid
-
-        # Potentially escaping the medium if this is the current medium's boundary
-        medium[active_surface & si.is_medium_transition()] = ref_interaction.target_medium(ds.d);
 
         ray = ref_interaction.spawn_ray(ds.d)
         total_dist = mi.Float(0.0)
